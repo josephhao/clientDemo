915f53fe87b7d5c664c2afed2aa861fff00d4791
josephhao <joseph-hao@hotmail.com>
2020-08-05 09:54:32 +0800

    m

---
+++ websocket-client/src/test/java/com/cw/client/websocket/WebsocketClientApplicationTests.java
@@ -0,0 +1,21 @@
+package com.cw.client.websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CompletableFuture;
+
+@SpringBootTest
+class WebsocketClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
